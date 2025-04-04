c033af68c1d37b44c90c95b7d1e955efa5e83b90
lavkumarsingh <lavkumarsingh11@gmail.com>
2025-04-04 11:51:23 +0530

    github pages configuration files

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/Assignment',
+  assetPrefix: '/Assignment/',
+}
+
+module.exports = nextConfig
